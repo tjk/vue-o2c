9f14820b4aba9eeaa1a3c62fa45e1a753f0bbab8
TJ Koblentz <tj.koblentz@gmail.com>
2022-10-23 14:22:04 -0700

    handle error better when run with cli

--- src/cli.ts
+++ src/cli.ts
@@ -6,8 +6,13 @@ if (!process.argv[2]) {
   console.error("usage: vue-o2c </path/to/sfc.vue>")
   process.exit(1)
 }
-const state = transformPath(path.resolve(process.argv[2]))
-if (!state.transformed) {
+try {
+  const state = transformPath(path.resolve(process.argv[2]))
+  if (!state.transformed) {
+    process.exit(1)
+  }
+  console.log(state.transformed)
+} catch (e) {
+  console.error(e)
   process.exit(1)
 }
-console.log(state.transformed)
